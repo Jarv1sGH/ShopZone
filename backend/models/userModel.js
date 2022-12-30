ee61254004d5143e0b4783dd070fd847fe29a3b4
Jarv1sGH <itsmejarvis007@gmail.com>
2022-12-30 12:23:35 +0530

    s

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -48,7 +48,7 @@ userSchema.pre("save", async function (next) {
 // JSON WEB TOKEN
 userSchema.methods.getJWTToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: "7 days",
+    expiresIn: process.env.JWT_EXPIRE,
   });
 };
 
