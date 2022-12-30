7062db1363c7c50102ddbc5434b033e047fb59cf
Jarv1sGH <itsmejarvis007@gmail.com>
2022-12-30 13:27:21 +0530

    s

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -12,28 +12,28 @@ const userSchema = new Schema({
     maxLength: [25, "Name cannot exceed 30 characters"],
     minLength: [3, "Name should contain atleast 3 characters"],
   },
-
+  
   email: {
     type: String,
     required: [true, "Please enter your Email"],
     unique: true,
     validate: [validator.isEmail, "Please enter a valid email address"],
   },
-
+  
   password: {
     type: String,
     required: [true, "Please enter your password"],
     minLength: [8, "Password should conatain atleast 8 characters"],
     select: false,
   },
-
+  
   role: {
     type: String,
     default: "user",
   },
-
+  
   resetPasswordToken: String,
-
+  
   resetPasswordExpire: Date,
 });
 
@@ -41,14 +41,14 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
   }
-
+  
   this.password = await bcrypt.hash(this.password, 10);
 });
 
 // JSON WEB TOKEN
 userSchema.methods.getJWTToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn:Number( process.env.JWT_EXPIRE),
+    expiresIn:"7d"
   });
 };
 
@@ -61,12 +61,12 @@ userSchema.methods.comparePassword = async function (Password) {
 userSchema.methods.getResetPasswordToken = function () {
   //Token Generation
   const resetToken = crypto.randomBytes(20).toString("hex");
-
+  
   // Hashing using "sha256"{Secure Hash Algorithm}  and adding to userSchema
   this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+  .createHash("sha256")
+  .update(resetToken)
+  .digest("hex");
   this.resetPasswordExpire = Date.now() + 15 * 60 * 1000; // converting to milliseconds
   return resetToken;
 };
