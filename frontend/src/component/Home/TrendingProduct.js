9a31d2127095345a0e3ab85b28e26e82d3eafd53
Jarv1sGH <itsmejarvis007@gmail.com>
2023-04-16 12:55:11 +0530

    removed lint errors

--- frontend/src/component/Home/TrendingProduct.js
+++ frontend/src/component/Home/TrendingProduct.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 import ImageGallery from "react-image-gallery";
