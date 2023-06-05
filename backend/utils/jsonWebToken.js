c337d31868102fca28247ecab746f71fe6ce10a9
Jarv1sGH <itsmejarvis007@gmail.com>
2023-06-05 21:39:42 +0530

    fixed Conversion of cookie expire time

--- backend/utils/jsonWebToken.js
+++ backend/utils/jsonWebToken.js
@@ -4,7 +4,9 @@ const sendToken = (user, statusCode, res) => {
 
   //Cookie options
   const options = {
-    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 1000),
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true,
   };
 
